c373643de9f7c41e26908888ad27dd80991be8f0
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-04 19:23:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,8 +12,8 @@ class GetRequester
   end 
  
   def get_response_body
-    url = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-     uri = URI.parse(url)
+    URL = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+     uri = URI.parse(URL)
      response = Net::HTTP.get_response(uri)
      response.body
   end 
