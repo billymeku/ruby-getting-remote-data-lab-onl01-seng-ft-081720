99158e0007b2a89419c957c85d4a5f4973b1efd8
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-04 19:12:53 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,12 +9,11 @@ class GetRequester
  def initialize(url)
    @url = url
    
-  URL ="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json."
- end 
- 
+#   URL ="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json."
+# end 
  
  def get_response_body
-  URL.open.string
+
  end 
   
   def parse_json
